5e17627ddbc88e372ca23ddd221271075f974515
K1berman <Kiberman73@gmail.com>
2024-05-09 03:47:00 +0300

    Version 1.2

--- school21_parser.py
+++ school21_parser.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-from config import URL, TOKEN, LOGIN, PASSWORD, TG_USER_ID
+from config import URL, TOKEN, LOGIN, PASSWORD, TG_USER_ID, TG_GROUP_ID
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,17 +12,20 @@ from bs4 import BeautifulSoup
 def make_authorization(driver: object, URL: str, LOGIN:str, PASSWORD: str) -> bool:
     try:
         driver.get(URL)
-        time.sleep(10)
+
+        wait = WebDriverWait(driver, 120)
+        wait.until(EC.presence_of_element_located((By.NAME, 'username')))
 
         username_input = driver.find_element(By.NAME, 'username')
         password_input = driver.find_element(By.NAME, 'password')
         username_input.send_keys(LOGIN)
         password_input.send_keys(PASSWORD)
 
-        login_button = WebDriverWait(driver, 10).until(
+        login_button = WebDriverWait(driver, 120).until(
             EC.element_to_be_clickable((By.XPATH, '//button[contains(@class, "rcx-button--primary")]'))
         )
         login_button.click()
+        print("Авторизация прошла успешно!")
         return True
 
     except Exception as error:
@@ -37,8 +40,10 @@ def check_changes(post_id: str, last_post_html:str) -> str | None:
                 try:
                     file.write(post_id)
                     header = last_post_html.find("div", class_="rcx-box rcx-box--full rcx-message-header")
-                    message = last_post_html.find("div", class_="rcx-message-body")
-                    data = f"{header.text}\n{message.text}"
+                    message = last_post_html.find("div", class_="rcx-message-body").find("div", class_="rcx-css-9nv4z9")
+
+                    data = f"{message.text.strip()}" if header is None else f"{header.text}\n{message.text.strip()}"
+
                     return data
                 except Exception as error:
                     print(f"Ошибка: {error}")
@@ -58,6 +63,8 @@ def find_last_post(HTML_CODE: str) -> tuple[str, str] | None:
             post_id = last_rcx_message.get("id")
 
         last_post = last_rcx_message.find("div", "rcx-box rcx-box--full rcx-message-container")
+        if last_post is None:
+            print("Последний пост не найден!")
         return (last_post, post_id)
     except Exception as error:
         print(f"Ошибка: {error}")
@@ -67,33 +74,44 @@ def find_last_post(HTML_CODE: str) -> tuple[str, str] | None:
 async def main() -> bool:
     bot = Bot(token=TOKEN)
     await bot.send_message(text="Бот запущен!", chat_id=TG_USER_ID)
-    driver = webdriver.Chrome()
+
+    options = webdriver.ChromeOptions()
+
+    driver = webdriver.Chrome(options=options)
 
     if not make_authorization(driver, URL, LOGIN, PASSWORD):
         print("Ошибка авторизации!")
         await bot.send_message(text="Ошибка авторизации!", chat_id=TG_USER_ID)
+        await bot.session.close()
         return 0
 
     while (True):
         try:
-            time.sleep(120)
+            time.sleep(10)
             driver.refresh()
-            time.sleep(5)
+            wait = WebDriverWait(driver, 360)
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "rcx-message")))
 
             HTML_CODE = driver.page_source
 
             last_post, post_id = find_last_post(HTML_CODE)
+
             if not last_post or not post_id:
                 print("Ошибка парсера!")
+                await bot.session.close()
                 return 0
 
             data = check_changes(post_id, last_post)
             if data:
                 await bot.send_message(text=data, chat_id=TG_USER_ID)
-
+                if TG_GROUP_ID:
+                    await bot.send_message(text=data, chat_id=TG_GROUP_ID)
         except Exception as error:
             print(f"Ошибка: {error}")
             await bot.send_message(text = "Ошибка парсера!", chat_id=TG_USER_ID)
+            driver.close()
+            driver.quit()
+            await bot.session.close()
             return 0
 
 
